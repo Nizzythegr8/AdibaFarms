1bf7945aa9aa32b6dcc6fc459dfb61eed1f568e7
Nizzythegr8 <78368502+Nizzythegr8@users.noreply.github.com>
2021-03-29 09:43:43 +0100

    Fixing about page

--- resources/js/script.js
+++ resources/js/script.js
@@ -41,4 +41,37 @@ $(document).ready(function() {
 
         }
     });
+
+    /*SROLL ON BUTTONS*/
+
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body')
+            .animate({
+                scrollTop: $('.js--section--plans')
+                    .offset().top
+            }, 1000);
+    });
+    $('.js--scroll-to-start').click(function() {
+        $('html, body')
+            .animate({
+                scrollTop: $('.js--section--features')
+                    .offset().top
+            }, 1000);
+    });
+
+    /*NAVIGATIONS*/
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 6000);
+                    return false;
+                }
+            }
+        });
+    });
 });
